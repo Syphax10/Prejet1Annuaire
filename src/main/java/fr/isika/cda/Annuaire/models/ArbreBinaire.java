66d12ee93040671aa5006aa12eec6d8e83e40745
Banouh Syphax <syphaxbanouh05@gmail.com>
2023-01-25 15:30:45 +0100

    Initialisation du git

---
+++ src/main/java/fr/isika/cda/Annuaire/models/ArbreBinaire.java
@@ -0,0 +1,14 @@
+package fr.isika.cda.Annuaire.models;
+
+public class ArbreBinaire {
+	
+	private GestionFichiers rafFichierDom ;
+	
+	public ArbreBinaire(String nomFichier) throws FileNotFoundEexception {
+		rafFichierDom = new GestionFichiers(nomFichier);
+	}
+	
+	// Les methodes
+	public void ajouterUnStagiare(Stagiaire)
+
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,4 @@
+module fr.isika.cda.Annuaire {
+    requires javafx.controls;
+    exports fr.isika.cda.Annuaire;
+}
