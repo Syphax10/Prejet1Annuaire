6a33b5d40e0dfbf74c0094cbc7dd4ecdaa2971af
Banouh Syphax <syphaxbanouh05@gmail.com>
2023-01-26 00:36:50 +0100

    Initialisation du git

--- src/main/java/fr/isika/cda/Annuaire/models/ArbreBinaire.java
+++ src/main/java/fr/isika/cda/Annuaire/models/ArbreBinaire.java
@@ -1,14 +1,95 @@
 package fr.isika.cda.Annuaire.models;
 
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.io.IOException;
+import java.util.List;
+
 public class ArbreBinaire {
 	
 	private GestionFichiers rafFichierDom ;
 	
-	public ArbreBinaire(String nomFichier) throws FileNotFoundEexception {
+	public ArbreBinaire(String nomFichier) throws FileNotFoundException {
 		rafFichierDom = new GestionFichiers(nomFichier);
 	}
 	
 	// Les methodes
-	public void ajouterUnStagiare(Stagiaire)
+	public void addStagiare(Stagiaire stagiaireToAdd) throws IOException {
+		if (rafFichierDom.getRaf().length() != 0) {
+			rafFichierDom.getRaf().seek(0);
+			Noeud firstNoeud = rafFichierDom.readNoeud();
+			firstNoeud.addNoeud(stagiaireToAdd, rafFichierDom);
+		} else {
+			rafFichierDom.getRaf().seek(0);
+			Noeud firstNoeud = new Noeud(stagiaireToAdd, -1, 0);
+			firstNoeud.writeNoeudBinaire(rafFichierDom.getRaf());
+		}
+	}
+	
+	public void odreAlphabetique(List<Stagiaire> stagiaireList) throws IOException {
+		if (rafFichierDom.getRaf().length() != 0) {
+			rafFichierDom.getRaf().seek(0);
+			Noeud firstNoeud = rafFichierDom.readNoeud();
+			firstNoeud.ordreAlphabetique(stagiaireList, rafFichierDom);
+		}
+	}
+	
+	//Recherche Stagiaire 
+	public void searchStagiaire(List<Stagiaire>resultatList,Stagiaire stagiaireSearch) throws IOException {
+		if (rafFichierDom.getRaf().length() != 0) {
+			rafFichierDom.getRaf().seek(0);
+			Noeud firstNoeud = rafFichierDom.readNoeud();
+			firstNoeud.searchStagiaire(resultatList,stagiaireSearch, rafFichierDom);
+		} else {
+			System.out.println("Ce fichier ne contient aucun stagiaire");
+		}
+	}
+	
+	 public void dbtRechAv(List<Stagiaire> searchAvancee ,Stagiaire stagiaireToFind) throws IOException {
+	        List<Stagiaire> ordreAlph = new ArrayList<>();
 
-}
+	        if (rafFichierDom.getRaf().length() != 0) {
+	            rafFichierDom.getRaf().seek(0);
+	            Noeud firstNoeud = rafFichierDom.readNoeud();
+	            firstNoeud.ordreAlphabetique(ordreAlph, rafFichierDom);
+	            for(Stagiaire stagiaire : ordreAlph) {
+	                stagiaire.searchAvancee(searchAvancee, stagiaireToFind);
+	            }
+	        } else {
+	            //retour visuel, fichier vide
+	            System.out.println("Aucun stagiaire ne correspond aux infomation fourni.");
+	        }
+	    }
+	 //Supprimer stagiaire
+	    public void supprimerUnStagiaire(Stagiaire stagiaireToDelete) throws IOException {
+	        if (rafFichierDom.getRaf().length() != 0) {
+	            rafFichierDom.getRaf().seek(0);
+	            Noeud firstNoeud = rafFichierDom.readNoeud();
+	            firstNoeud.deleteNoeud(stagiaireToDelete, rafFichierDom);
+	        }
+	    }
+
+	    public  void modification(Stagiaire stagiaireToEdit, Stagiaire stagiaireUpdated)
+	            throws IOException {
+
+	        if (stagiaireToEdit.compareTo(stagiaireUpdated) != 0) {
+	            if (rafFichierDom.getRaf().length() != 0) {
+	                addStagiaire(stagiaireUpdated);
+	                deleteStagiaire(stagiaireToEdit);
+	            }
+	        }
+
+	    }
+
+	    public GestionFichiers getRafFichierDom() {
+	        return rafFichierDom;
+	    }
+
+	    public void setRafFichierDom(GestionFichiers rafFichierDom) {
+	        this.rafFichierDom = rafFichierDom;
+	    }
+	
+	}
+
+
+
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,4 +1,5 @@
 module fr.isika.cda.Annuaire {
     requires javafx.controls;
+	requires javafx.graphics;
     exports fr.isika.cda.Annuaire;
 }
